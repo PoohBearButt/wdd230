ce44267b59bb497dcf8ff2558d7b522176440871
Elisha Wong <93792458+PoohBearButt@users.noreply.github.com>
2023-09-17 03:14:36 -0600

    going to bed

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,22 @@
-function getYear(){
-    const currentYear = new Date();
-    let yearNum = d.getFullYear();
-    consol.log:(yearNumb);
-    return 0;
-}
+const currentYear = new Date();
+console.log(currentYear);
+let chicken = currentYear.getFullYear();
+console.log(chicken);
+
+
+
+const dateModded = new Date(document.lastModified);
+//let dateTrim = dateModded.getDate();
+document.getElementById("lastModified").innerHTML = dateModded;
+
+
+//let datePull = document.getElementById("lastModified");
+//let currentMod = document.lastModified;
+//datePull.innerHTML = currentMod;
+
+document.getElementById("display").innerHTML = chicken;
+
+
+
+
+
